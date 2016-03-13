7c20e5f9ad9e358bf000fe4b2da462f9a7dd7e37
Valentin <valentinstoqnov98@gmail.com>
2016-03-13 20:15:15 +0200

    Buttons and onClickListeners added

--- app/src/main/java/org/elsys/valiolucho/businessmonefy/HomeActivity.java
+++ app/src/main/java/org/elsys/valiolucho/businessmonefy/HomeActivity.java
@@ -1,7 +1,10 @@
 package org.elsys.valiolucho.businessmonefy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        onClickButtonListener();
     }
+
+    //make SHOW GRAPHICS and SHOW LOGS buttons, initialization and set onClickListener
+    private static Button showGraphicsButton;
+    private static Button showLogsButton;
+
+    public void onClickButtonListener(){
+        showGraphicsButton = (Button) findViewById(R.id.buttonShowGraphics);
+        showGraphicsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent graphicsActivityIntent = new Intent(HomeActivity.this, GraphicsActivity.class);﻿
+                startActivity(graphicsActivityIntent);
+            }
+        });
+
+        showLogsButton = (Button) findViewById(R.id.buttonShowLogs);
+        showLogsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logsActivityIntent = new Intent(HomeActivity.this, LogsActivity.class);﻿
+                startActivity(logsActivityIntent);
+            }
+        });
+    }
+
 }
