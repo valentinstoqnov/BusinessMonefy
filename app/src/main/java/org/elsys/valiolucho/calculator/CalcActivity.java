b0b73f1c57ce2235dd86218fdf88fe7970a692a5
Valentin <valentinstoqnov98@gmail.com>
2016-04-04 17:57:23 +0300

    Calculator implementation started ...

---
+++ app/src/main/java/org/elsys/valiolucho/calculator/CalcActivity.java
@@ -0,0 +1,17 @@
+package org.elsys.valiolucho.calculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+import org.elsys.valiolucho.businessmonefy.R;
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+
+}
