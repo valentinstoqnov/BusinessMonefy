512c38754b54e2a6d65369d6917a4c56bf1f8615
Valentin <valentinstoqnov98@gmail.com>
2016-06-01 14:21:25 +0300

    commit

--- app/src/main/java/org/elsys/valiolucho/businessmonefy/MainActivity.java
+++ app/src/main/java/org/elsys/valiolucho/businessmonefy/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
